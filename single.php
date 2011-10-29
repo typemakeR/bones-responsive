9600fda7b94786de1ee55d07f87a25f8ac3c141b
eddiemachado <machado08@gmail.com>
2011-10-29 10:29:20 -0500

    added new <data> and itemscope to posts
    just doing it on single and page for now, let's wait and see how it
    progresses…
    for more info:
    http://www.whatwg.org/specs/web-apps/current-work/multipage/text-level-s
    emantics.html#the-data-element

--- single.php
+++ single.php
@@ -8,29 +8,29 @@
 					
 						<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 						
-						<article id="post-<?php the_ID(); ?>" <?php post_class('clearfix'); ?> role="article">
-							
-							<header>
-								
-								<h1 class="single-title"><?php the_title(); ?></h1>
-								
-								<p class="meta"><?php _e("Posted", "bonestheme"); ?> <time datetime="<?php echo the_time('Y-m-j'); ?>" pubdate><?php the_time('F jS, Y'); ?></time> <?php _e("by", "bonestheme"); ?> <?php the_author_posts_link(); ?> <span class="amp">&</span> <?php _e("filed under", "bonestheme"); ?> <?php the_category(', '); ?>.</p>
-							
-							</header> <!-- end article header -->
+						<article id="post-<?php the_ID(); ?>" <?php post_class('clearfix'); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
 						
-							<section class="post_content clearfix">
-								<?php the_content(); ?>
-								
-						
-							</section> <!-- end article section -->
+						<header>
 							
-							<footer>
+							<h1 class="single-title" itemprop="headline"><?php the_title(); ?></h1>
+							
+							<p class="meta"><?php _e("Posted", "bonestheme"); ?> <data itemprop="datePublished" value="<?php echo the_time('Y-m-j'); ?>"><?php the_time('F jS, Y'); ?></data> <?php _e("by", "bonestheme"); ?> <?php the_author_posts_link(); ?> <span class="amp">&</span> <?php _e("filed under", "bonestheme"); ?> <?php the_category(', '); ?>.</p>
+						
+						</header> <!-- end article header -->
 					
-								<?php the_tags('<p class="tags"><span class="tags-title">Tags:</span> ', ', ', '</p>'); ?>
-								
-							</footer> <!-- end article footer -->
+						<section class="post_content clearfix" itemprop="articleBody">
+							<?php the_content(); ?>
+							
+					
+						</section> <!-- end article section -->
 						
-						</article> <!-- end article -->
+						<footer>
+			
+							<?php the_tags('<p class="tags"><span class="tags-title">Tags:</span> ', ', ', '</p>'); ?>
+							
+						</footer> <!-- end article footer -->
+					
+					</article> <!-- end article -->
 						
 						<?php comments_template(); ?>
 						
