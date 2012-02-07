d75039325b321ff4bf26dc86edf6f9ff909744bb
eddiemachado <machado08@gmail.com>
2012-02-07 11:13:40 -0500

    remove Chrome Frame
    eff IE6

--- footer.php
+++ footer.php
@@ -17,12 +17,6 @@
 		<!-- scripts are now optimized via Modernizr.load -->	
 		<script src="<?php echo get_template_directory_uri(); ?>/library/js/scripts.js"></script>
 		
-		<!-- prompt for Google Chrome Frame for IE6 users -->
-		<!--[if lt IE 7 ]>
-  			<script src="//ajax.googleapis.com/ajax/libs/chrome-frame/1.0.3/CFInstall.min.js"></script>
-  			<script>window.attachEvent('onload',function(){CFInstall.check({mode:'overlay'})})</script>
-		<![endif]-->
-		
 		<?php wp_footer(); // js scripts are inserted using this function ?>
 
 	</body>
