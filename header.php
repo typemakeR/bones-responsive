8105d888452bac7d576cda01634ebd4d418d753e
eddiemachado <machado08@gmail.com>
2012-02-07 11:10:56 -0500

    cleaned up html tag for hb5 3.0
    also replaced viewport
    removed ie6 toolbar removal
    updated jQuery call

--- header.php
+++ header.php
@@ -1,11 +1,9 @@
 <!doctype html>  
-
 <!--[if IEMobile 7 ]> <html <?php language_attributes(); ?> class="no-js iem7"> <![endif]-->
-<!--[if lt IE 7 ]> <html <?php language_attributes(); ?> class="no-js ie6 oldie"> <![endif]-->
-<!--[if IE 7 ]>    <html <?php language_attributes(); ?> class="no-js ie7 oldie"> <![endif]-->
-<!--[if IE 8 ]>    <html <?php language_attributes(); ?> class="no-js ie8 oldie"> <![endif]-->
-<!--[if (gte IE 9)|(gt IEMobile 7)|!(IEMobile)|!(IE)]><!--><html <?php language_attributes(); ?> class="no-js"><!--<![endif]-->
-	
+<!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7" <?php language_attributes(); ?>> <![endif]-->
+<!--[if IE 7]>    <html class="no-js lt-ie9 lt-ie8" <?php language_attributes(); ?>> <![endif]-->
+<!--[if IE 8]>    <html class="no-js lt-ie9" <?php language_attributes(); ?>> <![endif]-->
+<!--[if (gte IE 8)|(gt IEMobile 7)|!(IEMobile)|!(IE)]><!--><html class="no-js" <?php language_attributes(); ?>><!--<![endif]-->
 	<head>
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
@@ -15,9 +13,8 @@
 		<!-- meta tags should be handled by SEO plugin. I reccomend (http://yoast.com/wordpress/seo/) -->
 		
 		<!-- mobile optimized -->
-		<meta name="viewport" content="width=device-width,initial-scale=1">
-		<!-- IE6 toolbar removal -->
-		<meta http-equiv="imagetoolbar" content="false" />
+		<meta name="viewport" content="width=device-width">
+		
 		<!-- allow pinned sites -->
 		<meta name="application-name" content="<?php bloginfo('name'); ?>" />
 		
@@ -28,11 +25,11 @@
 		<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/library/css/base.css">		
 		
 		<!-- Grab Google CDN's jQuery, with a protocol relative URL; fall back to local if necessary -->
-		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
-		<script>window.jQuery || document.write(unescape('%3Cscript src="<?php echo get_template_directory_uri(); ?>/library/js/libs/jquery-1.7.1.min.js"%3E%3C/script%3E'))</script>
+  		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+		<script>window.jQuery || document.write('<script src="<?php echo get_template_directory_uri(); ?>/library/js/libs/jquery-1.7.1.min.js"><\/script>')</script>
 		
 		<!-- modernizr (without media query polyfill) -->
-		<script src="<?php echo get_template_directory_uri(); ?>/library/js/modernizr.full.min.js"></script>
+		<script src="<?php echo get_template_directory_uri(); ?>/library/js/libs/modernizr.custom.min.js"></script>
 		
   		<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
 		
